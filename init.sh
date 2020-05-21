7b067345fb9680a196361661f4cd58823932db0b
r1phord <vlad.gorenkov20@gmail.com>
2020-05-21 20:46:25 +0000

    fix script and config

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo rm /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
