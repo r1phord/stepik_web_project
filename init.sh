a40eb36002173aa120eac40fde246b5f3f1b8144
r1phord <vlad.gorenkov.1996@mail.ru>
2020-05-26 14:10:57 +0200

    modified script and gitignore

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/default
+sudo /etc/inid.d/gunicorn restart
