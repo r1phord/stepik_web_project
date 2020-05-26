7aa111ebcbb2d639dd6ea714a78a7fc229a2a48c
r1phord <vlad.gorenkov.1996@mail.ru>
2020-05-26 14:17:43 +0200

    modified wsgi application

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 def wsgi(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    parameters = environ['QUERY_STRING'].split('&')
-    return '\n'.join(parameters)
+    if environ['REQUEST_METHOD'] == 'GET':
+        parameters = environ['QUERY_STRING'].split('&')
+        return '\n'.join(parameters)
