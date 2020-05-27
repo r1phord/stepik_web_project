f2bd68ad34aa0124dacb3995d81cb57aff250187
r1phord <vlad.gorenkov.1996@mail.ru>
2020-05-27 10:32:39 +0000

    some fixes

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def wsgi(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     if environ['REQUEST_METHOD'] == 'GET':
-        parameters = environ['QUERY_STRING'].split('&')
-        body = [bytes(i + '\n', 'ascii') for i in parameters]
-        return body
+    	parameters = environ['QUERY_STRING'].split('&')
+    	body = [bytes(i + '\n', 'ascii') for i in parameters]
+    	return body
     # return [bytes('Hello world', 'ascii')]
